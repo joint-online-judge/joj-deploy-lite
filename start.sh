e6ac7cbda8c79b05835fd8bea3cdbcecefe595f3
BoYanZh <bomingzh@sjtu.edu.cn>
2022-01-17 23:36:44 +0800

    fix: remove prod, add lite

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-USAGE="usage: $0 [prod|dev|stage]"
+USAGE="usage: $0 [lite|dev|stage]"
 
 check_env_vars() {
     if [ -f .env ]; then
@@ -13,9 +13,9 @@ check_env_vars() {
     echo "HORSE_SRC=$HORSE_SRC"
 }
 
-if [[ $1 == "prod" || $1 == "dev" || $1 == "stage" ]]; then
+if [[ $1 == "lite" || $1 == "dev" || $1 == "stage" ]]; then
     docker-compose pull
-    if [[ $1 != "prod" ]]; then
+    if [[ $1 != "lite" ]]; then
         check_env_vars
     fi
 elif [[ $1 == "help" ]]; then
@@ -27,8 +27,8 @@ else
 fi
 
 case "$1" in
-"prod")
-    docker-compose -f docker-compose.yml -f docker-compose-ui.yml -f docker-compose-prod.yml up -d
+"lite")
+    docker-compose -f docker-compose.yml up -d
     ;;
 "dev")
     docker-compose -f docker-compose.yml -f docker-compose-dev.yml -f docker-compose-ui.yml -p joj2 up -d
