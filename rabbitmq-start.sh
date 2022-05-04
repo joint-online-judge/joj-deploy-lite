49e903330fed86a79462e9c8b12e3d43cc7ed878
BoYanZh <bomingzh@sjtu.edu.cn>
2022-05-04 16:03:13 +0800

    feat: show commnds on rabbitmq startup

--- rabbitmq-start.sh
+++ rabbitmq-start.sh
@@ -2,11 +2,13 @@
 
 set -e
 set -u
+set -x
 
 
 
 (
     sleep 5;
+    echo "rabbitmq startup events..."
     #rabbitmqctl wait --timeout 60 /var/lib/rabbitmq/mnesia/rabbitmq.pid
     rabbitmqctl await_startup;
     rabbitmqctl add_user $RABBITMQ_USERNAME $RABBITMQ_PASSWORD;
@@ -15,6 +17,7 @@ set -u
     rabbitmqctl set_permissions -p $RABBITMQ_VHOST $RABBITMQ_USERNAME ".*" ".*" ".*";
 ) &
 
+echo "starting rabbitmq..."
 echo "management_agent.disable_metrics_collector = false" > /etc/rabbitmq/conf.d/management_agent.disable_metrics_collector.conf
 rabbitmq-plugins enable --offline rabbitmq_management
 rabbitmq-server $@
