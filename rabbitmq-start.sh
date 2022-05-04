9d67cd3f49de635d0b941efeee26be8db4dae963
BoYanZh <bomingzh@sjtu.edu.cn>
2022-05-04 14:45:06 +0800

    revert: rabbitmq-start.sh

--- rabbitmq-start.sh
+++ rabbitmq-start.sh
@@ -4,12 +4,19 @@ set -e
 set -u
 
 
+
+(
+    sleep 5;
+    #rabbitmqctl wait --timeout 60 /var/lib/rabbitmq/mnesia/rabbitmq.pid
+    rabbitmqctl await_startup;
+    rabbitmqctl add_user $RABBITMQ_USERNAME $RABBITMQ_PASSWORD;
+    rabbitmqctl set_user_tags $RABBITMQ_USERNAME administrator;
+    rabbitmqctl add_vhost $RABBITMQ_VHOST;
+    rabbitmqctl set_permissions -p $RABBITMQ_VHOST $RABBITMQ_USERNAME ".*" ".*" ".*";
+) &
+
 echo "management_agent.disable_metrics_collector = false" > /etc/rabbitmq/conf.d/management_agent.disable_metrics_collector.conf
 rabbitmq-plugins enable --offline rabbitmq_management
 rabbitmq-server $@
-#rabbitmqctl wait --timeout 60 /var/lib/rabbitmq/mnesia/rabbitmq.pid
-rabbitmqctl await_startup;
-rabbitmqctl add_user $RABBITMQ_USERNAME $RABBITMQ_PASSWORD;
-rabbitmqctl set_user_tags $RABBITMQ_USERNAME administrator;
-rabbitmqctl add_vhost $RABBITMQ_VHOST;
-rabbitmqctl set_permissions -p $RABBITMQ_VHOST $RABBITMQ_USERNAME ".*" ".*" ".*";
+
+
