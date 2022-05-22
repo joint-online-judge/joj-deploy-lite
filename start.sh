e7948451c4783c8c741739ff2bb2352cbc162435
BoYanZh <bomingzh@sjtu.edu.cn>
2022-05-22 23:18:23 +0800

    fix: take lite back for cicd

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-USAGE="usage: $0 [dev|stage]"
+USAGE="usage: $0 [lite|dev|stage]"
 
 check_env_vars() {
     if [ -f .env ]; then
@@ -15,8 +15,10 @@ check_env_vars() {
     echo "TIGER_SRC=$TIGER_SRC"
 }
 
-if [[ $1 == "dev" || $1 == "stage" ]]; then
-    check_env_vars
+if [[ $1 == "lite" || $1 == "dev" || $1 == "stage" ]]; then
+    if [[ $1 != "lite" ]]; then
+        check_env_vars
+    fi
 elif [[ $1 == "help" ]]; then
     echo $USAGE
     exit 0
@@ -26,6 +28,10 @@ else
 fi
 
 case "$1" in
+"lite")
+    docker-compose -f docker-compose.yml -f docker-compose-lite.yml pull
+    docker-compose -f docker-compose.yml -f docker-compose-lite.yml up -d
+    ;;
 "dev")
     docker-compose -f docker-compose.yml -f docker-compose-dev.yml -f docker-compose-ui.yml pull
     docker-compose -f docker-compose.yml -f docker-compose-dev.yml -f docker-compose-ui.yml -p joj2 up -d --force-recreate
